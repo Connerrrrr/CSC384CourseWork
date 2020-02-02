36e73b5477c55a38dbdaddc9b97f653451bc4455
Siyang Shen <siyang.shen@mail.utoronto.ca>
2020-02-02 00:17:36 -0500

    First commit

---
+++ A1/search/solution.py
@@ -0,0 +1,117 @@
+#   Look for #IMPLEMENT tags in this file. These tags indicate what has
+#   to be implemented to complete the warehouse domain.
+
+#   You may add only standard python imports---i.e., ones that are automatically
+#   available on TEACH.CS
+#   You may not remove any imports.
+#   You may not import or otherwise source any of your own files
+
+import os  # for time functions
+from search import *  # for search engines
+from sokoban import SokobanState, Direction, PROBLEMS  # for Sokoban specific classes and problems
+
+
+def sokoban_goal_state(state):
+    '''
+  @return: Whether all boxes are stored.
+  '''
+    for box in state.boxes:
+        if box not in state.storage:
+            return False
+    return True
+
+
+def heur_manhattan_distance(state):
+    # IMPLEMENT
+    '''admissible sokoban puzzle heuristic: manhattan distance'''
+    '''INPUT: a sokoban state'''
+    '''OUTPUT: a numeric value that serves as an estimate of the distance of the state to the goal.'''
+    # We want an admissible heuristic, which is an optimistic heuristic.
+    # It must never overestimate the cost to get from the current state to the goal.
+    # The sum of the Manhattan distances between each box that has yet to be stored and the storage point nearest to it is such a heuristic.
+    # When calculating distances, assume there are no obstacles on the grid.
+    # You should implement this heuristic function exactly, even if it is tempting to improve it.
+    # Your function should return a numeric value; this is the estimate of the distance to the goal.
+    distance_sum = 0
+    for box in state.boxes:
+        distances = list()
+        for storage in state.storage:
+            curr_dis = (abs(box[0] - storage[0]) + abs(box[1] - storage[1]))
+            distances.append(curr_dis)
+        if len(distances) > 0:
+            distance_sum += min(distances)
+    return distance_sum
+
+
+# SOKOBAN HEURISTICS
+def trivial_heuristic(state):
+    '''trivial admissible sokoban heuristic'''
+    '''INPUT: a sokoban state'''
+    '''OUTPUT: a numeric value that serves as an estimate of the distance of the state (# of moves required to get) to the goal.'''
+    count = 0
+    for box in state.boxes:
+        if box not in state.storage:
+            count += 1
+    return count
+
+
+def heur_alternate(state):
+    # IMPLEMENT
+    '''a better heuristic'''
+    '''INPUT: a sokoban state'''
+    '''OUTPUT: a numeric value that serves as an estimate of the distance of the state to the goal.'''
+    # heur_manhattan_distance has flaws.
+    # Write a heuristic function that improves upon heur_manhattan_distance to estimate distance between the current state and the goal.
+    # Your function should return a numeric value for the estimate of the distance to the goal.
+    return 0
+
+
+def heur_zero(state):
+    '''Zero Heuristic can be used to make A* search perform uniform cost search'''
+    return 0
+
+
+def fval_function(sN, weight):
+    # IMPLEMENT
+    """
+    Provide a custom formula for f-value computation for Anytime Weighted A star.
+    Returns the fval of the state contained in the sNode.
+
+    @param sNode sN: A search node (containing a SokobanState)
+    @param float weight: Weight given by Anytime Weighted A star
+    @rtype: float
+    """
+
+    # Many searches will explore nodes (or states) that are ordered by their f-value.
+    # For UCS, the fvalue is the same as the gval of the state. For best-first search, the fvalue is the hval of the state.
+    # You can use this function to create an alternate f-value for states; this must be a function of the state and the weight.
+    # The function must return a numeric f-value.
+    # The value will determine your state's position on the Frontier list during a 'custom' search.
+    # You must initialize your search engine object as a 'custom' search engine if you supply a custom fval function.
+    return 0
+
+
+def anytime_weighted_astar(initial_state, heur_fn, weight=1., timebound=10):
+    # IMPLEMENT
+    '''Provides an implementation of anytime weighted a-star, as described in the HW1 handout'''
+    '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
+    '''OUTPUT: A goal state (if a goal is found), else False'''
+    '''implementation of weighted astar algorithm'''
+    return False
+
+
+def anytime_gbfs(initial_state, heur_fn, timebound=10):
+    # IMPLEMENT
+    '''Provides an implementation of anytime greedy best-first search, as described in the HW1 handout'''
+    '''INPUT: a sokoban state that represents the start state and a timebound (number of seconds)'''
+    '''OUTPUT: A goal state (if a goal is found), else False'''
+    '''implementation of weighted astar algorithm'''
+    search_engine = SearchEngine(strategy="best_first")
+    search_engine.init_search(initial_state, sokoban_goal_state, heur_fn)
+
+    end_time = os.times()[0] + timebound
+
+
+    while os.times()[0] < end_time:
+        a = 1
+    return False
