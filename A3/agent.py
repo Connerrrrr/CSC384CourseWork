c4caf58ac16fef020ea7e5f897849a15fb4f0a11
Siyang Shen <siyang.shen@mail.utoronto.ca>
2020-03-08 19:53:33 -0400

    Reformatted

--- A3/agent.py
+++ A3/agent.py
@@ -9,29 +9,35 @@ import time
 # You can use the functions in othello_shared to write your AI
 from othello_shared import find_lines, get_possible_moves, get_score, play_move
 
-def eprint(*args, **kwargs): #you can use this for debugging, as it will print to sterr and not stdout
+
+def eprint(*args, **kwargs):  # you can use this for debugging, as it will print to sterr and not stdout
     print(*args, file=sys.stderr, **kwargs)
-    
+
+
 # Method to compute utility value of terminal state
 def compute_utility(board, color):
-    #IMPLEMENT
-    return 0 #change this!
+    # IMPLEMENT
+    return 0  # change this!
+
 
 # Better heuristic value of board
-def compute_heuristic(board, color): #not implemented, optional
-    #IMPLEMENT
-    return 0 #change this!
+def compute_heuristic(board, color):  # not implemented, optional
+    # IMPLEMENT
+    return 0  # change this!
+
 
 ############ MINIMAX ###############################
-def minimax_min_node(board, color, limit, caching = 0):
-    #IMPLEMENT
-    return ((0,0),0)
+def minimax_min_node(board, color, limit, caching=0):
+    # IMPLEMENT
+    return ((0, 0), 0)
 
-def minimax_max_node(board, color, limit, caching = 0): #returns highest possible utility
-    #IMPLEMENT
-    return ((0,0),0)
 
-def select_move_minimax(board, color, limit, caching = 0):
+def minimax_max_node(board, color, limit, caching=0):  # returns highest possible utility
+    # IMPLEMENT
+    return ((0, 0), 0)
+
+
+def select_move_minimax(board, color, limit, caching=0):
     """
     Given a board and a player color, decide on a move. 
     The return value is a tuple of integers (i,j), where
@@ -44,19 +50,22 @@ def select_move_minimax(board, color, limit, caching = 0):
     If caching is ON (i.e. 1), use state caching to reduce the number of state evaluations.
     If caching is OFF (i.e. 0), do NOT use state caching to reduce the number of state evaluations.    
     """
-    #IMPLEMENT
-    return (0,0) #change this!
+    # IMPLEMENT
+    return (0, 0)  # change this!
+
 
 ############ ALPHA-BETA PRUNING #####################
-def alphabeta_min_node(board, color, alpha, beta, limit, caching = 0, ordering = 0):
-    #IMPLEMENT
-    return ((0,0),0) #change this!
+def alphabeta_min_node(board, color, alpha, beta, limit, caching=0, ordering=0):
+    # IMPLEMENT
+    return ((0, 0), 0)  # change this!
 
-def alphabeta_max_node(board, color, alpha, beta, limit, caching = 0, ordering = 0):
-    #IMPLEMENT
-    return ((0,0),0) #change this!
 
-def select_move_alphabeta(board, color, limit, caching = 0, ordering = 0):
+def alphabeta_max_node(board, color, alpha, beta, limit, caching=0, ordering=0):
+    # IMPLEMENT
+    return ((0, 0), 0)  # change this!
+
+
+def select_move_alphabeta(board, color, limit, caching=0, ordering=0):
     """
     Given a board and a player color, decide on a move. 
     The return value is a tuple of integers (i,j), where
@@ -71,8 +80,9 @@ def select_move_alphabeta(board, color, limit, caching = 0, ordering = 0):
     If ordering is ON (i.e. 1), use node ordering to expedite pruning and reduce the number of state evaluations. 
     If ordering is OFF (i.e. 0), do NOT use node ordering to expedite pruning and reduce the number of state evaluations. 
     """
-    #IMPLEMENT
-    return (0,0) #change this!
+    # IMPLEMENT
+    return (0, 0)  # change this!
+
 
 ####################################################
 def run_ai():
@@ -82,30 +92,38 @@ def run_ai():
     Then it repeatedly receives the current score and current board state
     until the game is over.
     """
-    print("Othello AI") # First line is the name of this AI
+    print("Othello AI")  # First line is the name of this AI
     arguments = input().split(",")
-    
-    color = int(arguments[0]) #Player color: 1 for dark (goes first), 2 for light. 
-    limit = int(arguments[1]) #Depth limit
-    minimax = int(arguments[2]) #Minimax or alpha beta
-    caching = int(arguments[3]) #Caching 
-    ordering = int(arguments[4]) #Node-ordering (for alpha-beta only)
 
-    if (minimax == 1): eprint("Running MINIMAX")
-    else: eprint("Running ALPHA-BETA")
+    color = int(arguments[0])  # Player color: 1 for dark (goes first), 2 for light.
+    limit = int(arguments[1])  # Depth limit
+    minimax = int(arguments[2])  # Minimax or alpha beta
+    caching = int(arguments[3])  # Caching
+    ordering = int(arguments[4])  # Node-ordering (for alpha-beta only)
 
-    if (caching == 1): eprint("State Caching is ON")
-    else: eprint("State Caching is OFF")
+    if minimax == 1:
+        eprint("Running MINIMAX")
+    else:
+        eprint("Running ALPHA-BETA")
 
-    if (ordering == 1): eprint("Node Ordering is ON")
-    else: eprint("Node Ordering is OFF")
+    if caching == 1:
+        eprint("State Caching is ON")
+    else:
+        eprint("State Caching is OFF")
 
-    if (limit == -1): eprint("Depth Limit is OFF")
-    else: eprint("Depth Limit is ", limit)
+    if ordering == 1:
+        eprint("Node Ordering is ON")
+    else:
+        eprint("Node Ordering is OFF")
 
-    if (minimax == 1 and ordering == 1): eprint("Node Ordering should have no impact on Minimax")
+    if limit == -1:
+        eprint("Depth Limit is OFF")
+    else:
+        eprint("Depth Limit is ", limit)
 
-    while True: # This is the main loop
+    if minimax == 1 and ordering == 1: eprint("Node Ordering should have no impact on Minimax")
+
+    while True:  # This is the main loop
         # Read in the current game status, for example:
         # "SCORE 2 2" or "FINAL 33 31" if the game is over.
         # The first number is the score for player 1 (dark), the second for player 2 (light)
@@ -114,23 +132,24 @@ def run_ai():
         dark_score = int(dark_score_s)
         light_score = int(light_score_s)
 
-        if status == "FINAL": # Game is over.
+        if status == "FINAL":  # Game is over.
             print
         else:
-            board = eval(input()) # Read in the input and turn it into a Python
-                                  # object. The format is a list of rows. The
-                                  # squares in each row are represented by
-                                  # 0 : empty square
-                                  # 1 : dark disk (player 1)
-                                  # 2 : light disk (player 2)
+            board = eval(input())  # Read in the input and turn it into a Python
+            # object. The format is a list of rows. The
+            # squares in each row are represented by
+            # 0 : empty square
+            # 1 : dark disk (player 1)
+            # 2 : light disk (player 2)
 
             # Select the move and send it to the manager
-            if (minimax == 1): #run this if the minimax flag is given
+            if minimax == 1:  # run this if the minimax flag is given
                 movei, movej = select_move_minimax(board, color, limit, caching)
-            else: #else run alphabeta
+            else:  # else run alphabeta
                 movei, movej = select_move_alphabeta(board, color, limit, caching, ordering)
-            
+
             print("{} {}".format(movei, movej))
+
 
 if __name__ == "__main__":
     run_ai()
